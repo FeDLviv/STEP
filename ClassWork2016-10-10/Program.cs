e84a768bcde9b1bf51fff36fe8e2b69177be43a0
FeDLviv <fed.lviv@gmail.com>
2016-10-11 16:15:25 +0300

    Simple change in ClassWork2016-10-10

--- ClassWork2016-10-10/Program.cs
+++ ClassWork2016-10-10/Program.cs
@@ -2,53 +2,86 @@
 using System.Text;
 using System.IO;
 
+//C# System.IO
 namespace ClassWork2016_10_10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***MEMORYSTREAM***");
-            //потік в пам'яті
-            MemoryStream memStr = new MemoryStream();
-            Console.WriteLine("CAPACITY={0}", memStr.Capacity);
+            //C# System
+            //0. Objects, MarshalByRefObjects
+            //C# System.IO
+            //1. Stream (FileStream, MemoryStream, BufferedStream)
+            //2. TextReader (StreamReader, StringReader), TextWriter (StreamWriter, StringWriter), BinaryReader, BinaryWriter
+            //3. FileSystemInfo (FileInfo, DirectoryInfo), Path, File, Directory
 
-            //з консолі в потік MemoryStream
-            byte[] buff = Encoding.Unicode.GetBytes(Console.ReadLine());
-            memStr.Write(buff, 0, buff.Length);
-            Console.WriteLine("CAPACITY={0}", memStr.Capacity);
+            const string PATH_TO_FILE = @"data.txt";
 
-            //записати в масив дані з потоку MemoryStream
-            byte[] arr = memStr.ToArray();
+            MemoryStream mStr=new MemoryStream();
+            byte[] buff = Encoding.UTF8.GetBytes(Console.ReadLine());
+            mStr.Write(buff, 0, buff.Length);
+            FileStream fStr = new FileStream(PATH_TO_FILE, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            mStr.WriteTo(fStr);
+            fStr.Close();
 
-            //записати в файл дані з MemoryStream
-            FileStream file = new FileStream("text.txt", FileMode.Create);
-            memStr.WriteTo(file);
+            StreamReader strRed = new StreamReader(PATH_TO_FILE);
+            string text=strRed.ReadToEnd();
+            
+            StringReader stringR = new StringReader(text);
+            Console.WriteLine(stringR.ReadLine());
 
-            Console.WriteLine("***STRINGWRITER/STRINGREADER***");
-            StringWriter sw = new StringWriter();
-            sw.WriteLine("This text: {0}", 1);
-            Console.WriteLine("Generate string: {0}", sw.ToString());
-
-            StringReader sr = new StringReader("text");
+            //(StreamWriter, StringWriter), BinaryReader, BinaryWriter
+            //(FileInfo, DirectoryInfo), Path, File, Directory
+          
 
 
-            Console.WriteLine("***BINARYREADER/BINARYWRITER***");
-            //бінарні потоки
-            FileStream f = new FileStream("bin.dat", FileMode.Create, FileAccess.Write);
-            BinaryWriter bw = new BinaryWriter(f);
-            //запис в файл bin.dat - int
-            bw.Write(1985);
-            //запис в файл bin.dat - string
-            bw.Write("hello");
-            bw.Close();
 
-            BinaryReader bt = new BinaryReader(new FileStream("bin.dat", FileMode.Open, FileAccess.Read));
-            Console.WriteLine(bt.ReadInt32());
-            Console.WriteLine(bt.ReadString());
 
-            Console.WriteLine("***EXAMPLE***");
-            Example.StartExample();
+
+            //Stream fstr = new FileStream(@"text.txt", FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+
+            //Console.WriteLine("***MEMORYSTREAM***");
+            ////потік в пам'яті
+            //MemoryStream memStr = new MemoryStream();
+            //Console.WriteLine("CAPACITY={0}", memStr.Capacity);
+
+            ////з консолі в потік MemoryStream
+            //byte[] buff = Encoding.Unicode.GetBytes(Console.ReadLine());
+            //memStr.Write(buff, 0, buff.Length);
+            //Console.WriteLine("CAPACITY={0}", memStr.Capacity);
+
+            ////записати в масив дані з потоку MemoryStream
+            //byte[] arr = memStr.ToArray();
+
+            ////записати в файл дані з MemoryStream
+            //FileStream file = new FileStream("text.txt", FileMode.Create);
+            //memStr.WriteTo(file);
+
+            //Console.WriteLine("***STRINGWRITER/STRINGREADER***");
+            //StringWriter sw = new StringWriter();
+            //sw.WriteLine("This text: {0}", 1);
+            //Console.WriteLine("Generate string: {0}", sw.ToString());
+
+            //StringReader sr = new StringReader("text");
+
+
+            //Console.WriteLine("***BINARYREADER/BINARYWRITER***");
+            ////бінарні потоки
+            //FileStream f = new FileStream("bin.dat", FileMode.Create, FileAccess.Write);
+            //BinaryWriter bw = new BinaryWriter(f);
+            ////запис в файл bin.dat - int
+            //bw.Write(1985);
+            ////запис в файл bin.dat - string
+            //bw.Write("hello");
+            //bw.Close();
+
+            //BinaryReader bt = new BinaryReader(new FileStream("bin.dat", FileMode.Open, FileAccess.Read));
+            //Console.WriteLine(bt.ReadInt32());
+            //Console.WriteLine(bt.ReadString());
+
+            //Console.WriteLine("***EXAMPLE***");
+            //Example.StartExample();
         }
     }
 }
