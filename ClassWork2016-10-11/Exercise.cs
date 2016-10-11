22394a6fdb0555184b055fa8b11068ea158f29f0
IT Step <it-step@hotmail.com>
2016-10-11 20:52:32 +0300

    Init ClassWork2016-10-11

---
+++ ClassWork2016-10-11/Exercise.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace ClassWork2016_10_11
+{
+    static class Exercise
+    {
+        public static void SearchMenu()
+        {
+            Console.WriteLine("Choose:");
+            Console.WriteLine("\t1.Search by name");
+            Console.WriteLine("\t2.Search by size");
+            Console.WriteLine("\t3.Search by create date");
+            Console.WriteLine("\t4.Search by access");
+
+            switch ((char)Console.Read())
+            {
+                case '1':
+                    Console.WriteLine("Enter file/directory name:");
+                    Console.ReadLine();
+                    findFilesAndDirectories(Console.ReadLine());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void findFilesAndDirectories(string search)
+        {
+            foreach (DriveInfo x in DriveInfo.GetDrives())
+            {
+                SubDirectories(x.Name, search);
+            }
+        }
+
+        private static void SubDirectories(string subDir, string search)
+        {
+            DirectoryInfo d = new DirectoryInfo(subDir);
+            foreach (DirectoryInfo i in d.GetDirectories(search))
+            {
+                try
+                {
+                    SubDirectories(i.Name, search);
+                }
+                catch
+                {
+                    //Console.WriteLine("eX");
+                }
+                
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ClassWork2016-10-11/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace ClassWork2016_10_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileSystemInfo
+            //FileInfo ,File (static)
+            //DirectoryInfo, Directory (static)
+            //DriveInfo
+
+            ////директорії
+            //DirectoryInfo dir = new DirectoryInfo(@"C:\MyDir");
+            //if (dir.Exists)
+            //{
+            //    Console.WriteLine("FULL NAME={0}\tNAME={1}\tROOT={2}",dir.FullName, dir.Name, dir.Root);
+            //    foreach (DirectoryInfo x in dir.GetDirectories())
+            //    {
+            //        Console.WriteLine("DIR - {0}", x);
+            //    }
+            //    //dir.GetFiles("*.txt", SearchOption.TopDirectoryOnly);
+            //    foreach (FileInfo x in dir.GetFiles())
+            //    {
+            //        Console.WriteLine("FILE - {0}", x.Name);
+            //    }
+            //}
+            //else
+            //{
+            //    dir.Create();
+            //    dir.CreateSubdirectory("MySubdir");
+            //    //Directory.CreateDirectory(@"C:\MyDir");
+            //    //Directory.CreateDirectory(@"C:\MyDir\MySubdir");
+            //}
+
+            ////файли
+            //FileInfo file = new FileInfo(@"C:\MyDir\my.txt");
+            //if (file.Exists)
+            //{
+            //    StreamReader strRead=file.OpenText();
+            //    Console.WriteLine(strRead.ReadLine());
+            //    string[] arr=File.ReadAllLines(@"C:\MyDir\my.txt");
+            //}
+            //else
+            //{
+            //    FileStream fStr = file.Open(FileMode.Create, FileAccess.Write, FileShare.None);
+            //    fStr.Close();
+
+            //    StreamWriter strWr=file.AppendText();
+            //    strWr.Write(Console.ReadLine());
+            //    strWr.Close();
+            //}
+
+            ////шлях
+            //string path=Path.Combine(@"C:\MyDir\", @"asd.txt");
+            //Console.WriteLine(path);
+
+            ////логічні диски
+            //DriveInfo[] arrDrive=DriveInfo.GetDrives();
+            //foreach (DriveInfo x in arrDrive)
+            //{
+            //    Console.WriteLine("NAME - {0}\tFORMAT={1}\tTOTAL SIZE={2}GB", x.Name, x.DriveFormat, ((x.TotalSize / 1024) / 1024) / 1024);
+            //}
+
+            Exercise.SearchMenu();
+        }
+    }
+}
