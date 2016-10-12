b164dc03b43fc672e5ca46322cfc59a60611b86b
IT Step <it-step@hotmail.com>
2016-10-12 20:51:55 +0300

    Init ClassWork2016-10-12

---
+++ ClassWork2016-10-12/Example.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ClassWork2016_10_12
+{
+    class Example
+    {
+        public static void StartExample()
+        {
+            List<Student> list = new List<Student>();
+            list.Add(new Bachelor() { Name = "Fedir", Years = 16});
+            list.Add(new Magister() { Name = "Danil", Years = 15 });
+            list.Add(new Magister() { Name = "Maxim", Years = 18 });
+            list.Add(new Bachelor() { Name = "Vladislav", Years = 17 });
+            list.Add(new Bachelor() { Name = "Pavlo", Years = 16 });
+
+            Type typeBase = typeof(Student);
+            ScholarshipAttribute[] arr = (ScholarshipAttribute[])typeBase.GetCustomAttributes(typeof(ScholarshipAttribute), false);
+            if (!arr[0].Exists)
+            {
+                Console.WriteLine("There are no students with scholarship");
+            }
+            else
+            {
+                //Type typeB = typeof(Bachelor);
+                //foreach (ScholarshipAttribute x in (ScholarshipAttribute[])typeB.GetCustomAttributes(typeof(ScholarshipAttribute), true))
+                //{
+                //    //знизу до гори (від нащадка до батьківського)
+                //    Console.WriteLine(x.Exists);
+                //}
+                Console.WriteLine("List of the students with scholarship:");
+                foreach (Student x in list)
+                {
+                    Bachelor temp = x as Bachelor;
+                    if (temp != null)
+                    {
+                        PrintToConsole(x);
+                    }
+                }
+            }
+        }
+
+        public static void PrintToConsole(object obj)
+        {
+            Type type=obj.GetType();
+            MethodInfo info = type.GetMethod("ToString", Type.EmptyTypes);
+            UpperAttribute[] arr = (UpperAttribute[])info.GetCustomAttributes(typeof(UpperAttribute), false);
+            if (arr.Length > 0 && arr[0].IsUpper)
+            {
+                Console.WriteLine(obj.ToString().ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple =true)]
+    class UpperAttribute : Attribute
+    {
+        private bool isUpper;
+
+        public UpperAttribute(bool value)
+        {
+            isUpper = value;
+        }
+
+        public bool IsUpper
+        {
+            get { return isUpper; }
+            set { isUpper = value; }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class ScholarshipAttribute : Attribute
+    {
+        public bool Exists { get; set; }
+    }
+
+    [Scholarship(Exists = true)]
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int Years { get; set; }
+
+        [Upper(false)]
+        public override string ToString()
+        {
+            return String.Format("Student: {0} - {1} years", Name, Years);
+        }
+    }
+
+    [Scholarship(Exists = true)]
+    class Bachelor : Student
+    {
+        public string Specialization { get; set; }
+
+        [Upper(true)]
+        public override string ToString()
+        {
+            return base.ToString()+" - bachelor";
+        }
+    }
+
+    [Scholarship(Exists = false)]
+    class Magister:Student
+    {
+        public string SuperSpecialization { get; set; }
+
+        [Upper(false)]
+        public override string ToString()
+        {
+            return base.ToString() + " - magister";
+        }
+    }
+}
---
+++ ClassWork2016-10-12/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reflection;
+
+namespace ClassWork2016_10_12
+{
+    //АТРИБУТИ
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            Type type = typeof(Point);
+            //ДЛЯ КЛАСА
+            //true - враховуються атрибути батьківських класів
+            //false - враховуються атрибути тільки даного класу
+            object[] arrAttribute = type.GetCustomAttributes(false);
+            //якщо знаєм, що атрибут один і знаєм його тип
+            //MyFirstAttribute[] arrAttribute = (MyFirstAttribute[])type.GetCustomAttributes(false);
+            foreach (object x in arrAttribute)
+            {
+                if (x is MyFirstAttribute)
+                {
+                    Console.WriteLine("Owner={0}, version={1}", ((MyFirstAttribute)x).Organization, ((MyFirstAttribute)x).Version);
+                }
+            }
+            //ДЛЯ МЕТОДА
+            MethodInfo info=type.GetMethod("ToString", BindingFlags.Public | BindingFlags.Instance);
+            foreach(MyFirstAttribute x in info.GetCustomAttributes(typeof(MyFirstAttribute), false))
+            {
+                Console.WriteLine("Owner={0}, version={1}", x.Organization, x.Version);
+            }
+
+            Example.StartExample();
+        }
+    }
+
+    //перший атрибут
+    //AttributeTargets.Class|AttributeTargets.Struct (декорувати можна класи та структури)
+    //AllowMultiple = true (декількома екземплярами можна декорувати)
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple =true)]
+    class MyFirstAttribute : Attribute
+    {
+        private string organization;
+        private int version;
+
+        public MyFirstAttribute(string organization)
+        {
+            Organization = organization;
+        }
+
+        public int Version
+        {
+            get { return version; }
+            set { version = value; }
+        }
+
+        public string Organization
+        {
+            get { return organization; }
+            private set { organization = value; }
+        }
+    }
+
+    //клас з нашим атрибутом з параметрами
+    [MyFirst("STEP CA", Version=1)]
+    class Point
+    {
+        //компілятор не пропусте, оскільки наш атрибут може декорувати тільки класи і структури
+        //[MyFirst("STEP CA", Version =2)]
+        public override string ToString()
+        {
+            return "POINT";
+        }
+    }
+}
