a920bb3edd2c45ddb84babd85e30eed0413bb809
FeDLviv <fed.lviv@gmail.com>
2016-10-16 17:12:08 +0300

    Test SOAP serialization /deserialization

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,48 +1,68 @@
 ﻿using System;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 using System.IO;
 
 namespace Serialization
 {
-    //бінарні серіалізація/десеріалізація, та обмеження при них
+    //бінарна та SOAP серіалізація/десеріалізація, та обмеження при них
     class Program
     {
         static void Main(string[] args)
         {
             //клас A, обмеження при серіалізації/десеріалізації прописуємо власноруч
             A a1 = new A(5,3);
-            A a2;
+            A a2, a3;
             BinaryFormatter binFor = new BinaryFormatter();
+            SoapFormatter soapFor = new SoapFormatter();
 
             Console.WriteLine("a1={0}", a1);
             using(FileStream file=new FileStream("A.dat", FileMode.Create, FileAccess.Write))
             {
                 binFor.Serialize(file, a1);
             }
+            using (FileStream file = new FileStream("A.soap", FileMode.Create, FileAccess.Write))
+            {
+                soapFor.Serialize(file, a1);
+            }
 
             using (FileStream file = new FileStream("A.dat", FileMode.Open, FileAccess.Read))
             {
                 a2 = (A)binFor.Deserialize(file);
             }
+            using (FileStream file = new FileStream("A.soap", FileMode.Open, FileAccess.Read))
+            {
+                a3 = (A)soapFor.Deserialize(file);
+            }
             Console.WriteLine("a2={0}", a2);
+            Console.WriteLine("a3={0}", a3);
             Console.WriteLine();
 
-            //клас B, обмеження при серіалізації/десеріалізації прописуємо fnhb,enjv
+            //клас B, обмеження при серіалізації/десеріалізації прописуємо атрибутом
             B b1 = new B(2, 7);
-            B b2;
+            B b2, b3;
            
             Console.WriteLine("b1={0}", b1);
             using (FileStream file = new FileStream("B.dat", FileMode.Create, FileAccess.Write))
             {
                 binFor.Serialize(file, b1);
             }
+            using (FileStream file = new FileStream("B.soap", FileMode.Create, FileAccess.Write))
+            {
+                soapFor.Serialize(file, b1);
+            }
 
             using (FileStream file = new FileStream("B.dat", FileMode.Open, FileAccess.Read))
             {
                 b2 = (B)binFor.Deserialize(file);
             }
+            using (FileStream file = new FileStream("B.soap", FileMode.Open, FileAccess.Read))
+            {
+                b3 = (B)soapFor.Deserialize(file);
+            }
             Console.WriteLine("b2={0}", b2);
+            Console.WriteLine("b3={0}", b3);
         }
     }
 
