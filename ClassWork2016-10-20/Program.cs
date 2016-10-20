b3db762337b5d4de04bd105841549dd178e8f9c1
FeDLviv <fed.lviv@gmail.com>
2016-10-20 15:16:13 +0300

    Init ClassWork2016-10-20

---
+++ ClassWork2016-10-20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassWork2016_10_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
